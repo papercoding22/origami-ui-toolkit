527490a2dedd1f37cf31cb54d65b888fd7209d33
papercoding22 <john.ly997@gmail.com>
2025-07-01 20:48:46 +0700

    Update TypeScript configuration and dependencies; remove unused CSS file

--- packages/ui-toolkit/vite.config.ts
+++ packages/ui-toolkit/vite.config.ts
@@ -1,16 +1,9 @@
 import { defineConfig } from 'vite'
-import dts from 'vite-plugin-dts'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    dts({
-      insertTypesEntry: true,
-      outDir: 'dist/types',
-    }),
-  ],
+  plugins: [react()],
   build: {
     lib: {
       entry: 'src/index.ts',
